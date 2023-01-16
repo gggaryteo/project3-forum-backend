ba2d7f835b4d300eec56e9009d0d07b8c71b47e5
gggaryteo <garyteoprograms@gmail.com>
2023-01-16 11:06:09 +0800

    made minor changes to naming

--- db/migrations/20230116025855-create-taglist.js
+++ db/migrations/20230116025855-create-taglist.js
@@ -9,10 +9,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      user_id: {
+      tag_id: {
         type: Sequelize.INTEGER,
         references: {
-          model: "Users",
+          model: "Tags",
           key: "id",
         },
       },
